dcc7b266e85024b7a12b69c7e0acce33b3cdad6f
Felipe Santos <felipe.santos11@hotmail.com>
2021-07-21 20:31:15 -0300

    h2 database, test profile, JPA

--- src/main/java/br/com/udemy/osudemy/entities/User.java
+++ src/main/java/br/com/udemy/osudemy/entities/User.java
@@ -2,10 +2,21 @@ package br.com.udemy.osudemy.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "user")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
