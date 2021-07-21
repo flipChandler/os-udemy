fee21efe0e0dbc69388a5faaeed0d1b1dc4d8ea4
Felipe Santos <felipe.santos11@hotmail.com>
2021-07-21 20:02:39 -0300

    commit inicial

---
+++ src/main/java/br/com/udemy/osudemy/OsUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.udemy.osudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/udemy/osudemy/OsUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.udemy.osudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
