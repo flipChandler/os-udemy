ae57a6e1cc23e42ac8dbde462f05f441293983f3
Felipe Santos <felipe.santos11@hotmail.com>
2021-07-24 19:51:49 -0300

    exception handler - delete

--- src/main/java/br/com/udemy/osudemy/services/UserService.java
+++ src/main/java/br/com/udemy/osudemy/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.com.udemy.osudemy.entities.User;
 import br.com.udemy.osudemy.repositories.UserRepository;
+import br.com.udemy.osudemy.services.exceptions.DatabaseException;
 import br.com.udemy.osudemy.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -31,7 +34,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User user) {
