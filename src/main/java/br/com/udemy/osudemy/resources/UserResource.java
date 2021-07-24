1be368482e723d1b2f9c4d11f72950a6b51a72fa
Felipe Santos <felipe.santos11@hotmail.com>
2021-07-24 12:39:34 -0300

    user insert

--- src/main/java/br/com/udemy/osudemy/resources/UserResource.java
+++ src/main/java/br/com/udemy/osudemy/resources/UserResource.java
@@ -1,13 +1,17 @@
 package br.com.udemy.osudemy.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.udemy.osudemy.entities.User;
 import br.com.udemy.osudemy.services.UserService;
@@ -28,6 +32,23 @@ public class UserResource {
 	@GetMapping("/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id){
 		return ResponseEntity.ok(service.findById(id));
+	}
+	
+	/*
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public User insert(@RequestBody User user) {
+		return service.insert(user);
+	}
+	*/
+	
+	// TRAZ ATÉ O LOCATION DO HEADERS | http://localhost:8080/users/3
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User user) {
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).body(user);
 	}
 	
 }
